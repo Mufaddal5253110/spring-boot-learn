0b1231de13e1fad1a6fc964d429101634b30efc3
Mufaddal5253110 <mufaddalshakir55@gmail.com>
2024-04-19 18:03:57 +0530

    Get Photo By Id

--- src/main/java/com/jetbrains/mufaddal/springlearn/PhotoCloneController.java
+++ src/main/java/com/jetbrains/mufaddal/springlearn/PhotoCloneController.java
@@ -1,14 +1,23 @@
 package com.jetbrains.mufaddal.springlearn;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class PhotoCloneController {
 
-    private List<Photo> db = List.of(new Photo("1", "firstImage.jpg"));
+    private Map<String, Photo> db = new HashMap<>(){{
+        put("1",new Photo("1", "firstImage.jpg"));
+    }};
 
     @GetMapping("/")
     public String helloWorld() {
@@ -16,7 +25,14 @@ public class PhotoCloneController {
     }
 
     @GetMapping("/photos")
-    public List<Photo> getPhotos() {
-        return db;
+    public Collection<Photo> get() {
+        return db.values();
+    }
+
+    @GetMapping("/photos/{id}")
+    public Photo get(@PathVariable String id) {
+        Photo photo = db.get(id);
+        if(photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photo;
     }
 }
