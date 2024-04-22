afb37ada227b88024ad3d837f46c48b20dd7f89a
Mufaddal5253110 <mufaddalshakir55@gmail.com>
2024-04-22 13:48:00 +0530

    create get method with json using servlet

--- src/main/java/com/jetbrains/mufaddal/ApplicationLauncher/ApplicationLauncher.java
+++ src/main/java/com/jetbrains/mufaddal/ApplicationLauncher/ApplicationLauncher.java
@@ -11,6 +11,7 @@ package com.jetbrains.mufaddal.ApplicationLauncher;
 //	}
 //
 //}
+import com.jetbrains.mufaddal.ApplicationLauncher.web.MyFancyPdfInvoicesServlet;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Wrapper;
@@ -25,7 +26,7 @@ public class ApplicationLauncher {
 		tomcat.getConnector();
 
 		Context ctx = tomcat.addContext("", null);
-		Wrapper servlet = Tomcat.addServlet(ctx, "myFirstServlet", new MyFirstServlet());
+		Wrapper servlet = Tomcat.addServlet(ctx, "myFirstServlet", new MyFancyPdfInvoicesServlet());
 		servlet.setLoadOnStartup(1);
 		servlet.addMapping("/*");
 
