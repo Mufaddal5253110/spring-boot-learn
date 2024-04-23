6c2352223760577b9395fd91817dd2b92c0b0438
Mufaddal5253110 <mufaddalshakir55@gmail.com>
2024-04-23 11:30:16 +0530

    regiter dispatcher servlet to forward req to controller

--- src/main/java/com/jetbrains/mufaddal/ApplicationLauncher/ApplicationLauncher.java
+++ src/main/java/com/jetbrains/mufaddal/ApplicationLauncher/ApplicationLauncher.java
@@ -11,25 +11,48 @@ package com.jetbrains.mufaddal.ApplicationLauncher;
 //	}
 //
 //}
+
+import com.jetbrains.mufaddal.ApplicationLauncher.context.MyFancyPdfInvoicesApplicationConfiguration;
 import com.jetbrains.mufaddal.ApplicationLauncher.web.MyFancyPdfInvoicesServlet;
+import jakarta.servlet.ServletContext;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class ApplicationLauncher {
 
-	public static void main(String[] args) throws LifecycleException {
+    public static void main(String[] args) throws LifecycleException {
 
-		Tomcat tomcat = new Tomcat();
-		tomcat.setPort(8088);
-		tomcat.getConnector();
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8088);
+        tomcat.getConnector();
+        Context tomcatCtx = tomcat.addContext("", null);
 
-		Context ctx = tomcat.addContext("", null);
-		Wrapper servlet = Tomcat.addServlet(ctx, "myFirstServlet", new MyFancyPdfInvoicesServlet());
-		servlet.setLoadOnStartup(1);
-		servlet.addMapping("/*");
+        WebApplicationContext appCtx = createApplicationContext(tomcatCtx.getServletContext());
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(appCtx);
+        Wrapper servlet = Tomcat.addServlet(tomcatCtx, "dispatcherServlet", dispatcherServlet);
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/*");
 
-		tomcat.start();
-	}
+        tomcat.start();
+    }
+
+    public static WebApplicationContext createApplicationContext(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(MyFancyPdfInvoicesApplicationConfiguration.class);
+        ctx.setServletContext(servletContext);
+        ctx.refresh();
+        ctx.registerShutdownHook();
+        return ctx;
+    }
 }
+
+/*
+ * The applicationContext you create will know about your @Controller and @GetMapping, because of @ComponentScanning.
+ * Your DispatcherServlet knows about your applicationContext, because you need to pass it into its constructor.
+ * Because of that, the DispatcherServlet is able to forward incoming requests to your controllers and back to the browser
+ * */
