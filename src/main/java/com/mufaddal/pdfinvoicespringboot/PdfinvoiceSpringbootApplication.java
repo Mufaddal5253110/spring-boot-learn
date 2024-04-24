3f9afeb362b91af782b75fae68df7c50f0f33486
Mufaddal5253110 <mufaddalshakir55@gmail.com>
2024-04-24 13:17:14 +0530

    Initializing spring boot pdf invoice project

---
+++ src/main/java/com/mufaddal/pdfinvoicespringboot/PdfinvoiceSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.mufaddal.pdfinvoicespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfinvoiceSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfinvoiceSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mufaddal/pdfinvoicespringboot/PdfinvoiceSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mufaddal.pdfinvoicespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfinvoiceSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
