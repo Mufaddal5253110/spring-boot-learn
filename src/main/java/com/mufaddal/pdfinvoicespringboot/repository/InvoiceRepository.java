71aac242a28fff8cda44263a6a288dfc2384f53c
Mufaddal5253110 <mufaddalshakir55@gmail.com>
2024-04-30 11:55:41 +0530

    update h2 databasse name and repository

--- src/main/java/com/mufaddal/pdfinvoicespringboot/repository/InvoiceRepository.java
+++ src/main/java/com/mufaddal/pdfinvoicespringboot/repository/InvoiceRepository.java
@@ -4,7 +4,9 @@ import com.mufaddal.pdfinvoicespringboot.model.Invoice;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InvoiceRepository extends CrudRepository<Invoice, String> {
 
     @Query("SELECT id, pdf_url, user_id, amount FROM INVOICES where user_id = :userId")
